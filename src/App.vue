f61fc100594d588334248bdd29890ee1ed4ec02c
梁山 <liangshan@192.168.0.108>
2019-08-09 02:32:10 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
     <div class="nav">
-      <h3>领域分类</h3>
-      <hr>
+      <div class="head">领域分类</div>
       <ul>
         <li v-for="(domain, index) in menus"
             :key="index">
@@ -34,345 +33,350 @@
   </div>
 </template>
 <script>
-  export default {
-    name: 'App',
-    components: {
-      AllSvgs: () => import('./Svgs')
-    },
-    data () {
-      return {
-        openedMenu: -1,
-        activeSubMenu: '',
-        currentParems: {},
-        menus: [
-          {
-            label: '用户信息&鉴权领域',
-            name: 'user',
-            children: [
-              {
-                label: '登录',
-                name: 'login',
-                params: {
-                  'username': {
-                    type: 'String',
-                    default: 'ls',
-                    label: '用户名'
-                  },
-                  'password': {
-                    type: 'String',
-                    default: '123123',
-                    label: '密码'
-                  }
-                }
-              },
-              {
-                label: '注册',
-                name: 'register',
-                params: {}
-              },
-              {
-                label: '获取用户详情',
-                name: 'getUserDetail',
-                params: {}
-              }
-            ]
-          },
-          {
-            label: '职位领域',
-            name: 'job',
-            children: [
-              {
-                label: '获取职位详情',
-                name: 'requestJobDetail',
-                params: {
-                  'number': {
-                    type: 'String',
-                    default: '',
-                    label: '职位编号'
-                  }
-                }
-              },
-              {
-                label: '获取相似职位',
-                name: 'requestSimilarJobs',
-                params: {
-                  'number': {
-                    type: 'String',
-                    default: '',
-                    label: '职位编号'
-                  }
-                }
-              },
-              {
-                label: '获取在招职位',
-                name: 'requestAreaJobs',
-                params: {
-                  'number': {
-                    type: 'String',
-                    default: '',
-                    label: '职位编号'
-                  }
-                }
-              },
-              {
-                label: '获取在招职位所在城市',
-                name: 'requestAreaJobCity',
-                params: {
-                  'number': {
-                    type: 'String',
-                    default: '',
-                    label: '职位编号'
-                  }
-                }
-              },
-              {
-                label: '收藏职位',
-                name: 'requestJobCollection',
-                params: {
-                  'number': {
-                    type: 'String',
-                    default: '',
-                    label: '职位编号'
-                  }
-                }
-              },
-              {
-                label: '投递职位',
-                name: 'requestJobDeliver',
-                params: {
-                  'number': {
-                    type: 'String',
-                    default: '',
-                    label: '职位编号'
-                  }
-                }
-              },
-              {
-                label: '获取职位投递状态',
-                name: 'requestJobDeliverStatus',
-                params: {
-                  'number': {
-                    type: 'String',
-                    default: '',
-                    label: '职位编号'
-                  }
-                }
-              },
-              {
-                label: '获取最佳雇主',
-                name: 'requestBestEmployer',
-                params: {
-                  'number': {
-                    type: 'String',
-                    default: '',
-                    label: '职位编号'
-                  }
-                }
-              },
-              {
-                label: '获取最佳雇主评论',
-                name: 'requestBestEmployerComment',
-                params: {
-                  'number': {
-                    type: 'String',
-                    default: '',
-                    label: '职位编号'
-                  }
-                }
-              },
-              {
-                label: '获取职位的小程序二维码',
-                name: 'requestJobMpQrcode',
-                params: {
-                  'number': {
-                    type: 'String',
-                    default: '',
-                    label: '职位编号'
-                  }
+export default {
+  name: 'App',
+  components: {
+    AllSvgs: () => import('./Svgs')
+  },
+  data () {
+    return {
+      openedMenu: -1,
+      activeSubMenu: '',
+      currentParems: {},
+      menus: [
+        {
+          label: '用户信息&鉴权领域',
+          name: 'user',
+          children: [
+            {
+              label: '登录',
+              name: 'login',
+              params: {
+                'username': {
+                  type: 'String',
+                  default: 'ls',
+                  label: '用户名'
+                },
+                'password': {
+                  type: 'String',
+                  default: '123123',
+                  label: '密码'
                 }
               }
-            ]
-          },
-          {
-            label: '公司领域',
-            name: 'company',
-            children: [
-
-            ]
-          },
-          {
-            label: '简历领域',
-            name: 'resume',
-            children: [
-
-            ]
-          },
-          {
-            label: '广告领域',
-            name: 'ads',
-            children: [
-
-            ]
-          },
-          {
-            label: '其它领域',
-            name: 'other',
-            children: [
-
-            ]
-          }
-        ]
-      }
-    },
-    created () {
-      this.openedMenu = this.findOpenedMenuIndex()
-      this.currentParems = this.findCurrentParams()
-    },
-    methods: {
-      toggleMenu (index) {
-        this.openedMenu = (this.openedMenu === index ? -1 : index)
-      },
-      findCurrentParams () {
-        let i = 0
-        let menus = this.menus
-        let outParams = []
-        for (i; i < menus.length; i++) {
-          if (menus[i].name != this.$route.params.domain) {
-            continue
-          } else {
-            for (let j = 0; j < menus[i].children.length; j++) {
-              if (menus[i].children[j].name !== this.$route.params.methods) {
-                continue
-              } else {
-                outParams = menus[i].children[j].params
-                j = menus[i].children.length
+            },
+            {
+              label: '注册',
+              name: 'register',
+              params: {}
+            },
+            {
+              label: '获取用户详情',
+              name: 'getUserDetail',
+              params: {}
+            }
+          ]
+        },
+        {
+          label: '职位领域',
+          name: 'job',
+          children: [
+            {
+              label: '获取职位详情',
+              name: 'requestJobDetail',
+              params: {
+                'number': {
+                  type: 'String',
+                  default: '',
+                  label: '职位编号'
+                }
               }
+            },
+            {
+              label: '获取相似职位',
+              name: 'requestSimilarJobs',
+              params: {
+                'number': {
+                  type: 'String',
+                  default: '',
+                  label: '职位编号'
+                }
+              }
+            },
+            {
+              label: '获取在招职位',
+              name: 'requestAreaJobs',
+              params: {
+                'number': {
+                  type: 'String',
+                  default: '',
+                  label: '职位编号'
+                }
+              }
+            },
+            {
+              label: '获取在招职位所在城市',
+              name: 'requestAreaJobCity',
+              params: {
+                'number': {
+                  type: 'String',
+                  default: '',
+                  label: '职位编号'
+                }
+              }
+            },
+            {
+              label: '收藏职位',
+              name: 'requestJobCollection',
+              params: {
+                'number': {
+                  type: 'String',
+                  default: '',
+                  label: '职位编号'
+                }
+              }
+            },
+            {
+              label: '投递职位',
+              name: 'requestJobDeliver',
+              params: {
+                'number': {
+                  type: 'String',
+                  default: '',
+                  label: '职位编号'
+                }
+              }
+            },
+            {
+              label: '获取职位投递状态',
+              name: 'requestJobDeliverStatus',
+              params: {
+                'number': {
+                  type: 'String',
+                  default: '',
+                  label: '职位编号'
+                }
+              }
+            },
+            {
+              label: '获取最佳雇主',
+              name: 'requestBestEmployer',
+              params: {
+                'number': {
+                  type: 'String',
+                  default: '',
+                  label: '职位编号'
+                }
+              }
+            },
+            {
+              label: '获取最佳雇主评论',
+              name: 'requestBestEmployerComment',
+              params: {
+                'number': {
+                  type: 'String',
+                  default: '',
+                  label: '职位编号'
+                }
+              }
+            },
+            {
+              label: '获取职位的小程序二维码',
+              name: 'requestJobMpQrcode',
+              params: {
+                'number': {
+                  type: 'String',
+                  default: '',
+                  label: '职位编号'
+                }
+              }
+            }
+          ]
+        },
+        {
+          label: '公司领域',
+          name: 'company',
+          children: [
+
+          ]
+        },
+        {
+          label: '简历领域',
+          name: 'resume',
+          children: [
+
+          ]
+        },
+        {
+          label: '广告领域',
+          name: 'ads',
+          children: [
+
+          ]
+        },
+        {
+          label: '其它领域',
+          name: 'other',
+          children: [
+
+          ]
+        }
+      ]
+    }
+  },
+  created () {
+    this.openedMenu = this.findOpenedMenuIndex()
+    this.currentParems = this.findCurrentParams()
+  },
+  methods: {
+    toggleMenu (index) {
+      this.openedMenu = (this.openedMenu === index ? -1 : index)
+    },
+    findCurrentParams () {
+      let i = 0
+      let menus = this.menus
+      let outParams = []
+      for (i; i < menus.length; i++) {
+        if (menus[i].name != this.$route.params.domain) {
+          continue
+        } else {
+          for (let j = 0; j < menus[i].children.length; j++) {
+            if (menus[i].children[j].name !== this.$route.params.methods) {
+              continue
+            } else {
+              outParams = menus[i].children[j].params
+              j = menus[i].children.length
             }
           }
         }
-        return outParams
-      },
-      findOpenedMenuIndex () {
-        let i = 0
-        let outIndex = -1
-        let menus = this.menus
-        for (i; i < menus.length; i++) {
-          if (this.$route.params.domain === menus[i].name) {
-            outIndex = i
-            i = menus.length
-          }
-        }
-        return outIndex
-      },
-      navigateTo (domain, methods, params, label) {
-        this.currentParems = params
-        document.title = label
-        this.$router.replace({
-          path: '/' + domain + '/' + methods
-        })
       }
+      return outParams
+    },
+    findOpenedMenuIndex () {
+      let i = 0
+      let outIndex = -1
+      let menus = this.menus
+      for (i; i < menus.length; i++) {
+        if (this.$route.params.domain === menus[i].name) {
+          outIndex = i
+          i = menus.length
+        }
+      }
+      return outIndex
+    },
+    navigateTo (domain, methods, params, label) {
+      this.currentParems = params
+      document.title = label
+      this.$router.replace({
+        path: '/' + domain + '/' + methods
+      })
     }
   }
+}
 </script>
 
 <style>
-  body,
-  html {
-    width: 100%;
-    height: 100%;
-    padding: 0;
-    margin: 0;
-  }
-  #app {
-    font-family: "Avenir", Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #2c3e50;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: row;
-  }
-  .nav {
-    width: 250px;
-    height: 100%;
-    background-color: #333;
-    box-shadow: 0 0 10px 2px #111;
-    z-index: 2;
-  }
-  .nav h3 {
-    padding: 0 15px;
-    box-sizing: border-box;
-    color: #c8c8c8;
-  }
-  .nav ul {
-    height: calc(100% - 100px);
-    overflow-y: auto;
-  }
-  .nav ul li {
-    list-style: none;
-  }
-  .nav ul li {
-    position: relative;
-    /* height: 48px; */
-    display: flex;
-    flex-direction: column;
-  }
-  .nav ul li > a {
-    line-height: 40px;
-    color: #ccc;
-    text-decoration: none;
-    transition: color 0.1s ease-in-out;
-  }
-  .nav ul li > a:hover {
-    color: #aaa;
-  }
-  .nav ul li > a.active {
-    color: #42b983 !important;
-  }
-  .nav ul {
-    padding-left: 30px;
-  }
-  .nav .sub_items {
-    width: 100%;
-    transition: all 0.25s cubic-bezier(0.215, 0.61, 0.355, 1);
-    transform-origin: 50% 0;
-    overflow: hidden;
-  }
-  .nav .sub_items .sub_item_inner {
-    box-sizing: border-box;
-    background-color: #383838;
-    border-radius: 5px;
-    box-shadow: 0 0 10px #303030 inset;
-  }
-  .nav .sub_items .sub_item_inner .sub_item > a {
-    display: flex;
-    font-size: 14px;
-    color: #888;
-    line-height: 40px;
-    text-decoration: none;
-    transition: all 0.1s linear;
-  }
-  .nav .sub_items .sub_item_inner .sub_item > a:hover {
-    color: #ccc;
-  }
-  .nav .sub_items .sub_item_inner .sub_item > a.active {
-    color: #42b983 !important;
-    font-size: 15px;
-  }
+body,
+html {
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+}
+.nav {
+  width: 250px;
+  height: 100%;
+  background-color: #333;
+  box-shadow: 0 0 10px 2px #111;
+  z-index: 2;
+}
+.nav .head {
+  padding: 0 15px;
+  box-sizing: border-box;
+  height: 66px;
+  color: #c8c8c8;
+  border-bottom: 1px solid #444;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.nav ul {
+  height: calc(100% - 100px);
+  overflow-y: auto;
+}
+.nav ul li {
+  list-style: none;
+}
+.nav ul li {
+  position: relative;
+  /* height: 48px; */
+  display: flex;
+  flex-direction: column;
+}
+.nav ul li > a {
+  line-height: 40px;
+  color: #ccc;
+  text-decoration: none;
+  transition: color 0.1s ease-in-out;
+}
+.nav ul li > a:hover {
+  color: #aaa;
+}
+.nav ul li > a.active {
+  color: #42b983 !important;
+}
+.nav ul {
+  padding-left: 30px;
+}
+.nav .sub_items {
+  width: 100%;
+  transition: all 0.25s cubic-bezier(0.215, 0.61, 0.355, 1);
+  transform-origin: 50% 0;
+  overflow: hidden;
+}
+.nav .sub_items .sub_item_inner {
+  box-sizing: border-box;
+  background-color: #383838;
+  border-radius: 5px;
+  box-shadow: 0 0 10px #303030 inset;
+}
+.nav .sub_items .sub_item_inner .sub_item > a {
+  display: flex;
+  font-size: 14px;
+  color: #888;
+  line-height: 40px;
+  text-decoration: none;
+  transition: all 0.1s linear;
+}
+.nav .sub_items .sub_item_inner .sub_item > a:hover {
+  color: #ccc;
+}
+.nav .sub_items .sub_item_inner .sub_item > a.active {
+  color: #42b983 !important;
+  font-size: 15px;
+}
 
-  .content {
-    flex: 1;
-    height: 100%;
-    overflow-y: auto;
-    /* padding: 15px; */
-    box-sizing: border-box;
-    display: flex;
-    flex-direction: row;
-    align-items: flex-start;
-  }
-  .content .inner {
-    width: 100%;
-    height: 100%;
-  }
+.content {
+  flex: 1;
+  height: 100%;
+  overflow-y: auto;
+  /* padding: 15px; */
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+}
+.content .inner {
+  width: 100%;
+  height: 100%;
+}
 </style>
--- src/Svgs.vue
+++ src/Svgs.vue
@@ -13,6 +13,134 @@
               id="play">
         <path d="M713.896 393.15l-240-158.691c-80.996-54.004-150-17.987-150 80.996v360c0 98.73 65.703 134.707 150 80.703l240-158.711c80.724-60 80.724-150 0-203.984v-0.313z"></path>
       </symbol>
+
+      <!-- loading: http://demo.lanrenzhijia.com/demo/49/4989/demo/   http://www.htmleaf.com/Demo/201501071122.html -->
+      <symbol viewBox="0 0 100 100"
+              id="loading2">
+        <path d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"
+              transform="rotate(281.881 50 50)">
+          <animateTransform attributeName="transform"
+                            attributeType="XML"
+                            type="rotate"
+                            dur="1s"
+                            from="0 50 50"
+                            to="360 50 50"
+                            repeatCount="indefinite"></animateTransform>
+        </path>
+      </symbol>
+
+      <symbol viewBox="0 0 44 44"
+              id="loading">
+        <g fill="none"
+           fill-rule="evenodd"
+           stroke-width="2">
+          <circle cx="22"
+                  cy="22"
+                  r="1">
+            <animate attributeName="r"
+                     begin="0s"
+                     dur="1.8s"
+                     values="1; 20"
+                     calcMode="spline"
+                     keyTimes="0; 1"
+                     keySplines="0.165, 0.84, 0.44, 1"
+                     repeatCount="indefinite" />
+            <animate attributeName="stroke-opacity"
+                     begin="0s"
+                     dur="1.8s"
+                     values="1; 0"
+                     calcMode="spline"
+                     keyTimes="0; 1"
+                     keySplines="0.3, 0.61, 0.355, 1"
+                     repeatCount="indefinite" />
+          </circle>
+          <circle cx="22"
+                  cy="22"
+                  r="1">
+            <animate attributeName="r"
+                     begin="-0.9s"
+                     dur="1.8s"
+                     values="1; 20"
+                     calcMode="spline"
+                     keyTimes="0; 1"
+                     keySplines="0.165, 0.84, 0.44, 1"
+                     repeatCount="indefinite" />
+            <animate attributeName="stroke-opacity"
+                     begin="-0.9s"
+                     dur="1.8s"
+                     values="1; 0"
+                     calcMode="spline"
+                     keyTimes="0; 1"
+                     keySplines="0.3, 0.61, 0.355, 1"
+                     repeatCount="indefinite" />
+          </circle>
+        </g>
+      </symbol>
+
+      <symbol viewBox="0 0 120 30"
+              id="loading-dots">
+        <circle cx="15"
+                cy="15"
+                r="15">
+          <animate attributeName="r"
+                   from="15"
+                   to="15"
+                   begin="0s"
+                   dur="0.8s"
+                   values="15;9;15"
+                   calcMode="linear"
+                   repeatCount="indefinite" />
+          <animate attributeName="fill-opacity"
+                   from="1"
+                   to="1"
+                   begin="0s"
+                   dur="0.8s"
+                   values="1;.5;1"
+                   calcMode="linear"
+                   repeatCount="indefinite" />
+        </circle>
+        <circle cx="60"
+                cy="15"
+                r="9"
+                fill-opacity="0.3">
+          <animate attributeName="r"
+                   from="9"
+                   to="9"
+                   begin="0s"
+                   dur="0.8s"
+                   values="9;15;9"
+                   calcMode="linear"
+                   repeatCount="indefinite" />
+          <animate attributeName="fill-opacity"
+                   from="0.5"
+                   to="0.5"
+                   begin="0s"
+                   dur="0.8s"
+                   values=".5;1;.5"
+                   calcMode="linear"
+                   repeatCount="indefinite" />
+        </circle>
+        <circle cx="105"
+                cy="15"
+                r="15">
+          <animate attributeName="r"
+                   from="15"
+                   to="15"
+                   begin="0s"
+                   dur="0.8s"
+                   values="15;9;15"
+                   calcMode="linear"
+                   repeatCount="indefinite" />
+          <animate attributeName="fill-opacity"
+                   from="1"
+                   to="1"
+                   begin="0s"
+                   dur="0.8s"
+                   values="1;.5;1"
+                   calcMode="linear"
+                   repeatCount="indefinite" />
+        </circle>
+      </symbol>
     </defs>
   </svg>
 </template>
